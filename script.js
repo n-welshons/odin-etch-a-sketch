4f03e37f8ce91328fee857175d757e928c5d63ee
Nathaniel <232973063+n-welshons@users.noreply.github.com>
2026-03-02 18:36:57 -0800

    Add buttons to reset and create new grids

--- script.js
+++ script.js
@@ -12,10 +12,41 @@ for (let i = 0; i < 16; i++) {
     }
     
 }
-const grid = document.querySelectorAll(".gridsquare")
+let grids = document.querySelectorAll(".gridsquare");
+const resetButton = document.getElementById("resetButton");
+const newGrid = document.getElementById("newGrid");
 function trail(event) {
     event.target.style.backgroundColor = "grey";
 }
-grid.forEach(div => {
+grids.forEach(div => {
     div.addEventListener("mouseenter", trail);
 });
+function reset() {
+    currentGrids = document.querySelectorAll(".gridsquare")
+    currentGrids.forEach (element => {
+        element.style.backgroundColor = "white";
+    });
+}
+resetButton.addEventListener("click", reset);
+function generateNewGrid () {
+    let size = prompt("How large of a grid do you want? Enter the number of tiles per side");
+    const oldGrid = document.getElementById("container");
+    oldGrid.replaceChildren();
+    for (let i = 0; i < size; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.id = i;
+        newDiv.className = "flexbox";
+        container.appendChild(newDiv);
+        for (let k = 0; k < size; k++) {
+            const row = document.getElementById(i);
+            const rowDiv = document.createElement("div");
+            rowDiv.className = "gridsquare";
+            row.appendChild(rowDiv);
+        }
+    }
+    grids = document.querySelectorAll(".gridsquare");
+    grids.forEach(div => {
+        div.addEventListener("mouseenter", trail);
+    });
+}
+newGrid.addEventListener("click", generateNewGrid);
